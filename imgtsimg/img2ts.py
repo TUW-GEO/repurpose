803b07fee204efe92f8a046edbb20483e9589cef
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2015-11-16 16:46:05 +0100

    Fix superfluous space in time unit
    - This breaks the reading of the data in some stupid readers.

--- imgtsimg/img2ts.py
+++ imgtsimg/img2ts.py
@@ -153,7 +153,7 @@ class Img2Ts(object):
                  r_methods='nn', r_weightf=None, r_min_n=1, r_radius=18000,
                  r_neigh=8, r_fill_values=None, filename_templ='%04d.nc',
                  gridname='grid.nc', global_attr=None, ts_attributes=None,
-                 ts_dtypes=None, time_units="days since  1858-11-17 00:00:00", zlib=False):
+                 ts_dtypes=None, time_units="days since 1858-11-17 00:00:00", zlib=False):
 
         self.imgin = input_dataset
         self.zlib = zlib
